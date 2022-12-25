c00c322bb2ce86f3162a9ebb31f68ea432fdf74d
Darja Rozonova <70627452+dariarozonova@users.noreply.github.com>
2022-12-25 17:44:36 +0200

    update login

--- pages/LogIn.vue
+++ pages/LogIn.vue
@@ -18,11 +18,11 @@
         class="mt-13 mx-auto"
         max-width="500">
         <v-card-title class="text-h6 justify-center font-weight-regular">
-        <span>{{ currentTitle }}</span>
+        <span>Reģistrācija</span>
         </v-card-title>
 
-        <v-window v-model="step">
-        <v-window-item :value="1">
+        <v-window>
+        <v-window-item>
             <v-card-text>
                 <v-text-field
                     v-model="email"
@@ -44,65 +44,44 @@
                     counter="20"
                     label="Uzvards"
                     value=""
-                  ></v-text-field>
-                  <v-select
-                  v-model="select"
-                  :items="items"
-                  :rules="[v => !!v || 'Šis lauks ir nepieciešams']"
-                  label="Kas jūs gribāt būt?"
-              ></v-select>
-            </v-card-text>
-        </v-window-item>
-
-            <v-window-item :value="2">
-            <v-card-text>
-            <v-text-field
-                v-model="password"
-                :rules="[required, min8]"
-                min="8"
-                label="Parole"
-                type="password"
-                required
-            ></v-text-field>
-            <v-text-field
-                v-model="password1"
-                :rules="[min8, matchingPasswords]"
-                label="Paroles apstiprināšana"
-                type="password"
-                required
-            ></v-text-field>
-            <span class="text-caption grey--text text--darken-1">
-                Ievadiet Jūsu paroli
-            </span>
-            </v-card-text>
-            </v-window-item>
-
-              <v-window-item :value="3">
-                <div class="pa-4 text-center">
-                <span class="text-caption grey--text">Paldies, ka lietojat BidBuild!</span>
-                </div>
-              </v-window-item>
-            </v-window>
+                ></v-text-field>
+                <v-select
+                    v-model="select"
+                    :items="items"
+                    :rules="[v => !!v || 'Šis lauks ir nepieciešams']"
+                    label="Kas jūs gribāt būt?"
+                ></v-select>
+                <v-text-field
+                    v-model="password"
+                    min="8"
+                    label="Parole"
+                    type="password"
+                    required
+                ></v-text-field>
+                <v-text-field
+                    v-model="password"
+                    label="Paroles apstiprināšana"
+                    type="password"
+                    required
+                ></v-text-field>
+                <span class="text-caption grey--text text--darken-1">
+                    Ievadiet Jūsu paroli
+                </span>
+              </v-card-text>
+              <v-card-actions>
+                  <v-btn
+                  outlined
+                  color="blue"
+                  >
+                  <Nuxt-link to="/PakalpojumuLists" target="_blank" style="text-decoration:none">
+                  Reģistreties
+                  </Nuxt-link>
+                </v-btn>
+              </v-card-actions>
+          </v-window-item>
+        </v-window>
 
         <v-divider></v-divider>
-
-        <v-card-actions>
-        <v-btn
-            :disabled="step === 1"
-            text
-            @click="step--"
-        >
-            Atgriezties atpakaļ
-        </v-btn>
-        <v-spacer></v-spacer>
-        <v-btn
-            :disabled="step === 3"
-            color="primary"
-            depressed
-            @click="step++">
-            Nakamais
-        </v-btn>
-        </v-card-actions>
     </v-card>
             <Footer />
         </div>
@@ -110,16 +89,7 @@
 
 <script>
   export default {
-    
     computed: {
-      currentTitle () {
-        switch (this.step) {
-          case 1: return 'Reģistrācija'
-          case 2: return 'Ievadiet paroli'
-          case 3: return 'Konts ir izveidots!'
-        }
-      },
-    },
     data: () => ({
       name: '',
       nameRules: [
@@ -129,7 +99,7 @@
       email: '',
       emailRules: [
         v => !!v || 'Šis lauks ir nepieciešams',
-        v => /.+@.+\..+/.test(v) || 'E-pastam jābūt pareiza formāta, piem. vards.uzvards@gmail.com',
+        v => !!v || /.+@.+\..+/.test(v) || 'E-pastam jābūt pareiza formāta, piem. vards.uzvards@gmail.com',
         v => (v && v.length <= 255) || 'E-pastam ir jābūt ne vairāk par 255 simboliem',
         v => !!v || (v && v.length > 7) || 'E-pastam ir jābūt minimāli 8 simboli',
       ],
@@ -150,8 +120,5 @@
       ],
     }),
   }
+  }
 </script>
-
-<style>
-
-</style>
